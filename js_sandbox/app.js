591e6a2c6fb0013591209d72f5bf13f2418ee8b6
b-random <brandon.brigance@gmail.com>
2022-08-06 12:15:06 -0700

    commit for elements selector

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -21,18 +21,87 @@
 
 // document.querySelector()
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+
+
+//multiple elements selectors
+
+
+// const items = document.getElementsByClassName('collection-item');
+
+//console.log(items[0].clientWidth);
+
+// items[0].style.color = 'blue';
+// items[0].textContent = 'blue';
+
+//getElementsByTagName
+
+// let lis = document.getElementsByTagName('li');
+
+// // function greener(){
+// //   for(i=0;i < lis.length; i++){
+// //   lis[i].style.color = 'green';
+// // }}       or...
+
+// lis = Array.from(lis);
+
+// lis.forEach(function(li){
+//   li.style.color = 'orange';
+// })
+
+//doc.querySelectorAll()
+
+const items = document.querySelectorAll('li');
+//console.log(items[1].style.color='purple');
+const odder = document.querySelectorAll('li:nth-child(odd)');
+const evener = document.querySelectorAll('li:nth-child(even)');
+
+odder.forEach(function(i){
+  i.style.color = 'green'
+});
+
+for(let x=0; x < evener.length; x++){
+  evener[x].style.background = 'grey';
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
